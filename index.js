6280a05e0e4a07706df00b1a1d2eaba53032bc9f
MayurSit2023 <mayur.c@shaligraminfotech.com>
2023-08-04 12:08:28 +0530

    Remove code from index.js

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ const Checkout = require('./dist/functions/checkout')
 const PaymentIntent = require('./dist/functions/paymentIntents')
 const PaymentMethod = require('./dist/functions/paymentmethod')
 
-Stripe.setStripeSecretKey('sk_test_51NaWpfSExVYHj4PxB7PSnpMwofCkbTOolNPV45SIYjoZi7DoA8AdK5kO86VszFVlHXlPOiK44LwtS1pqw6tsSxK200VPAla7UU')
-const custor = PaymentMethod.listCustomerPaymentMethods({
-    customerId:'cus_ONfaDJW0CEf9Xd',
-    // usage:'on_session'
-})
-custor.then(res => {
-    console.log(res);
-}).catch(err => {
-    console.log(err);
-})
 module.exports = {
     Stripe,
     Customer,
